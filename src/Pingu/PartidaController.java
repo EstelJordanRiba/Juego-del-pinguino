700930e0bb3a3cdd28dcb0d331b904d82424d364
Estel <esteljori777@alumnes.ilerna.com>
2026-03-09 18:52:58 +0100

    primera controller

---
+++ src/Pingu/PartidaController.java
@@ -0,0 +1,33 @@
+package Pingu;
+
+import model.*;
+
+public class PartidaController {
+
+    private Partida partida;
+
+    public PartidaController(Partida partida) {
+        this.partida = partida;
+    }
+
+    public void iniciarPartida() {
+        partida.iniciarPartida();
+    }
+
+    public Jugador obtenerJugadorActual() {
+        return partida.obtenirJugadorActual();
+    }
+
+    public void tirarDau(Dau dau) {
+        partida.jugarTornTirarDau(dau);
+    }
+
+    public boolean usarBolaNeu(Jugador objetivo) {
+        return partida.jugarTornBolaNeu(objetivo);
+    }
+
+    public void siguienteTurno() {
+        partida.seguentTorn();
+    }
+
+}
