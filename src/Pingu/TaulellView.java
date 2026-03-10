e61a187c7dab4b18affa5066d4293444d214de00
Estel <esteljori777@alumnes.ilerna.com>
2026-03-10 15:44:09 +0100

    TaulellView

---
+++ src/Pingu/TaulellView.java
@@ -0,0 +1,10 @@
+package Pingu;
+import model.Taulell;
+
+public class TaulellView {
+
+    public void mostrarTaulell(Taulell taulell) {
+        // TODO: representar el tablero en pantalla
+    }
+
+}
