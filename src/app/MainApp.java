f583ed0a55804bb5feae660873d2108b76bea224
Estel <esteljori777@alumnes.ilerna.com>
2026-03-19 16:26:32 +0100

    cambio del main

---
+++ src/app/MainApp.java
@@ -0,0 +1,27 @@
+	package app;
+
+	import javafx.application.Application;
+	import javafx.fxml.FXMLLoader;
+	import javafx.scene.Scene;
+	import javafx.stage.Stage;
+
+	public class MainApp extends Application {
+
+	    @Override
+	    public void start(Stage stage) throws Exception {
+	        FXMLLoader loader = new FXMLLoader(
+	                getClass().getResource("/fxml/menu.fxml")
+	        );
+
+	        Scene scene = new Scene(loader.load(), 900, 650);
+	        stage.setTitle("El Joc d'en Pingu");
+	        stage.setScene(scene);
+	        stage.show();
+	    }
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+	}
+	
+
