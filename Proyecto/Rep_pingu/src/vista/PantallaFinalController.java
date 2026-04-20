e5796297aed34164c81798a1aa9346669cad58da
Estel <esteljori777@alumnes.ilerna.com>
2026-04-20 19:16:41 +0200

    a

---
+++ Proyecto/Rep_pingu/src/vista/PantallaFinalController.java
@@ -0,0 +1,30 @@
+package vista;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import modelo.Jugador;
+
+public class PantallaFinalController {
+
+    @FXML
+    private Label textoGanador;
+
+    public void setGanador(Jugador j) {
+        textoGanador.setText(  j.getNombre() + "!");
+    }
+
+    @FXML
+    private void volverMenu() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/resources/PantallaMenu.fxml"));
+            Stage stage = (Stage) textoGanador.getScene().getWindow();
+            stage.setScene(new Scene(root));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
